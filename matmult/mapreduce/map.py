b066ac55606997fbb6e2a833d24100593d22c3d1
MrHohn <zzhonzi@gmail.com>
2015-09-30 23:16:49 -0400

    Finished the basic version of matrix multiply, pass local version and hadoop version, but currently a bit messed up on the order aspect

---
+++ matmult/mapreduce/map.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# in this mappper and reducer, I simply suppose M = 100, P = 100, N = 100
+# hence A -> M * P -> 100 * 100, B -> P * N -> 100 * 100
+
+import sys
+
+def main():
+    M, P, N = 100, 100, 100
+    # M, P, N = 4, 4, 4
+    # input comes from STDIN (standard input)
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # split the line into elements
+        matrix, row, column, val = line.split()
+        if matrix == 'A':
+            for i in xrange(0, N):
+                # first two elements represent the key pair, after are the needed messages, like this {(i, k), (A or B, j, value)}, k in 0 to N - 1
+                print '%s %s %s %s %s' % (row, i, matrix, column, val)
+        elif matrix == 'B':
+            for i in xrange(0, M):
+                # first two elements represent the key pair, after are the needed messages, like this {(k, j), (A or B, i, value)}, k in 0 to M - 1
+                print '%s %s %s %s %s' % (i, column, matrix, row, val)
+
+if __name__ == "__main__":
+    main()
---
+++ matmult/mapreduce/reduce.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# in this mappper and reducer, I simply suppose M = 100, P = 100, N = 100
+# hence A -> M * P -> 100 * 100, B -> P * N -> 100 * 100
+
+# from operator import itemgetter
+import sys
+
+def main():
+    # define the lastKeys to help detect the end of a certain block
+    lastKeyI, lastKeyJ = None, None
+    # define two dictionarys to store the needed value and identifier
+    dictA = {}
+    dictB = {}
+    # input comes from STDIN
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # parse the input we got from map.py
+        keyI, keyJ, matrix, identifier, val = line.split(' ')
+
+        #skip the first one
+        if lastKeyI:
+            # if the key pair changes, start calculate and output
+            if keyI != lastKeyI or keyJ != lastKeyJ:
+                # now calculate the current block's value
+                currentVal = 0;
+                for ident in dictA:
+                    # check if the identifier is also in dictB
+                    if ident in dictB:
+                        currentVal += dictA[ident] * dictB[ident]
+                # finish sum up, if sum isn't 0, then write it out
+                if currentVal != 0:
+                    print '%s %s %s' % (lastKeyI, lastKeyJ, currentVal)
+                # key pair is changed, record it
+                lastKeyI = keyI
+                lastKeyJ = keyJ
+                # clean up the dicts for new groups of values
+                dictA = {}
+                dictB = {}
+            # store the first element of the new group
+            if matrix == 'A':
+                dictA[identifier] = int(val)
+            else:
+                dictB[identifier] = int(val)
+        else: # store the key pair at the first time
+            if matrix == 'A':
+                dictA[identifier] = int(val)
+            else:
+                dictB[identifier] = int(val)                
+            lastKeyI = keyI
+            lastKeyJ = keyJ
+
+if __name__ == "__main__":
+    main()
